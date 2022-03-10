73227fec3bfd791d815a5ac28c7437b3a4b6b4ac
Arkiralor <pritho11335@gmail.com>
2022-03-10 18:33:25 +0530

    added new app for audio posts

---
+++ audiopostapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AudiopostappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'audiopostapp'
---
+++ audiopostapp/model_choices.py
@@ -0,0 +1,9 @@
+GENRE_CHOICES = (
+    ('Fiction', 'fiction'),
+    ('Non-Fiction', 'non-fiction'),
+    ('Poetry', 'poetry'),
+    ('Journal', 'journal')
+)
+
+if __name__ == "__main__":
+    pass
