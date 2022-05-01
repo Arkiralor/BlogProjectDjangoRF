b209cc13697962e3852c2fee68c1ecca5898bd64
Arkiralor <prithoo11335@gmail.com>
2022-05-01 22:43:29 +0530

    removed example code

--- auth/user_auth.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class HasKeyAuth(BasePermission):
-    '''
-    Allows access only to users who have been assigned keys.
-    '''
-    def has_permission(self, request, view):
-        return bool(request.user and request.user.has_key)
--- blogapp/views.py
+++ blogapp/views.py
@@ -5,7 +5,6 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
-from auth.user_auth import HasKeyAuth
 from userapp.models import Author, User
 from userapp.serializers import AuthorSerializer, UserAdminSerializer
 from .models import Blog, Tag
@@ -13,28 +12,6 @@ from .serializers import BlogPostSerializer, BlogInSerializer, BlogOutSerializer
 from .utils import TagUtils, LanguageUtils
 
 # Create your views here.
-
-class KeyAuthTestView(APIView):
-    '''
-    View to test the key-auth:
-    '''
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [HasKeyAuth]
-
-    def get(self, request):
-        '''
-        GET the key-auth test:
-        '''
-        user = request.user
-        serialized = UserAdminSerializer(user)
-        return Response(
-            {
-                "message": "You have successfully authenticated with the custom KeyAuth permission class.",
-                "user": serialized.data
-            },
-            status=status.HTTP_200_OK
-        )
-
 
 class BlogView(APIView):
     '''
