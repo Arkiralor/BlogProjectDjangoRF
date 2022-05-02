9cbf9a3988375c6737445fe6195944f16528f8d4
arkiralor <prithoo11335@gmail.com>
2022-05-02 18:37:24 +0530

    added new user permission in user_auth

--- auth/user_auth.py
+++ auth/user_auth.py
@@ -5,5 +5,19 @@ class HasKeyAuth(BasePermission):
     '''
     Allows access only to users who have been assigned keys.
     '''
+
     def has_permission(self, request, view):
         return bool(request.user and request.user.has_key)
+
+
+class IsModerator(BasePermission):
+    '''
+    Allows access only to moderators and superusers.
+    '''
+
+    def has_permission(self, request, view):
+        is_true = (
+            request.user.user_type == "moderator" 
+            or request.user.is_staff
+            )
+        return bool(is_true and request.user.is_authenticated)
