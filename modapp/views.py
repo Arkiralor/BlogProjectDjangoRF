a32626815363a6458bd6e60b886fd215810c84b4
arkiralor <prithoo11335@gmail.com>
2022-05-04 18:16:50 +0530

    removed unused imports

--- modapp/views.py
+++ modapp/views.py
@@ -3,12 +3,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 
-from blogapp.models import Blog
-from blogapp.serializers import BlogPostSerializer
 from modapp.models import ReportedPost
 from modapp.serializers import ReportedPostSerializer
-from userapp.models import User, Author
-from userapp.serializers import UserAdminSerializer, AuthorSerializer
 from auth.custom_permissions import IsModerator
 # Create your views here.
 
@@ -18,7 +14,7 @@ class GetAllReportedView(APIView):
     View to GET all reported posts:
     '''
     authentication_classes = (TokenAuthentication,)
-    # permission_classes = (IsModerator,)
+    permission_classes = (IsModerator,)
 
     def get(self, request):
         '''
